5853602ed4f234ca829c997f4c50c5dffebfd86e
Jose Salazar <jopasari@gmail.com>
2024-07-10 19:48:44 -0700

    Start foro alura challege

---
+++ src/test/java/foro/alura/demo/ForoAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package foro.alura.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
