c3d0d95b234716e9a8a1a2c01929a0e0fbdc9d69
Jose Salazar <jopasari@gmail.com>
2024-07-11 20:55:21 -0700

    Data based created and sync with the code

---
+++ src/main/java/foro/alura/demo/topico/DatosRegistroTopico.java
@@ -0,0 +1,6 @@
+package foro.alura.demo.topico;
+
+public record DatosRegistroTopico(String titulo, String mensaje, String fechaDeCreacion, String status, String autor, String curso) {
+
+
+}
---
+++ src/main/java/foro/alura/demo/topico/Topico.java
@@ -0,0 +1,38 @@
+package foro.alura.demo.topico;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "topico")
+@Entity(name = "Topico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Topico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String titulo;
+    private String mensaje;
+    private String fechaDeCreacion;
+    private String status;
+    private String autor;
+    private String curso;
+
+    public Topico(DatosRegistroTopico datosRegistroTopico){
+        this.titulo = datosRegistroTopico.titulo();
+        this.mensaje = datosRegistroTopico.mensaje();
+        this.fechaDeCreacion = datosRegistroTopico.fechaDeCreacion();
+        this.status = datosRegistroTopico.status();
+        this.autor = datosRegistroTopico.autor();
+        this.curso = datosRegistroTopico.curso();
+    }
+
+}
